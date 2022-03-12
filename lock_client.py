a568604eec66fa9f16c50e2cddbd571547194767
João Oliveira <61056525+jotanmiguel@users.noreply.github.com>
2022-03-12 14:51:16 +0000

    lock_client.py

--- lock_client.py
+++ lock_client.py
@@ -7,7 +7,33 @@ Números de aluno:
 """
 # Zona para fazer imports
 
-
+import sys
+import socket as s
+import net_client
 
 # Programa principal
 
+if len(sys.argv) > 1 and sys.argv[1] == 'localhost':
+    HOST = '127.0.0.1'
+    PORT = sys.argv[2]
+elif len(sys.argv) > 1:
+    HOST = sys.argv[1]
+    PORT = sys.argv[2]
+else:
+    HOST = '127.0.0.1'
+    PORT = 9999
+
+while True:
+    comandosSup = ['EXIT', 'LOCK', 'RELEASE'] # lista de comandos suportados
+
+
+    try:
+        inputLinha = input("comando > ") # l
+        args = inputLinha.split()
+        comando = args[2]
+
+
+
+    except KeyboardInterrupt:
+        print("\n KeyboardInterrupt")
+        exit()
