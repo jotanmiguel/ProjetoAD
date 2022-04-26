7c3c304c5430204ca6ca46426714b441075546c8
João Oliveira <skreibar2pt@gmail.com>
2022-04-26 16:12:53 +0100

    siga para bingo

---
+++ Entrega 3/cliente.py
@@ -0,0 +1,7 @@
+"""
+Aplicações distribuídas - Projeto 3 - cliente.py
+Grupo: 2
+Números de aluno: 45105, 45810, 46586
+"""
+
+import json
---
+++ Entrega 3/server.py
@@ -0,0 +1,7 @@
+"""
+Aplicações distribuídas - Projeto 3 - server.py
+Grupo: 2
+Números de aluno: 56908, 56954
+"""
+
+import json
